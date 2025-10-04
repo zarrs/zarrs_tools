f243540b7cc4ca4cbda86f7cd8f31761e71144f2
Lachy Deakin <ljdgit@gmail.com>
2025-10-05 09:02:15 +1100

    feat:  Add `--direct-io` flag to various binaries (#36)
    * feat:  Add `--direct-io` flag to `zarrs_benchmark_read_sync`
    
    * feat: add --direct-io to all relevant binaries
    
    * changelog
    
    * chore: bump `zarrs_filesystem` to 0.3.1 in lockfile

--- src/bin/zarrs_benchmark_read_sync.rs
+++ src/bin/zarrs_benchmark_read_sync.rs
@@ -11,7 +11,7 @@ use zarrs::{
         ArrayShardedReadableExtCache, ChunkRepresentation,
     },
     array_subset::ArraySubset,
-    filesystem::FilesystemStore,
+    filesystem::{FilesystemStore, FilesystemStoreOptions},
     storage::ReadableStorage,
 };
 use zarrs_tools::calculate_chunk_and_codec_concurrency;
@@ -44,6 +44,12 @@ struct Args {
     /// If set, checksum validation in codecs (e.g. crc32c) is skipped.
     #[arg(long, default_value_t = false)]
     ignore_checksums: bool,
+
+    /// Enable direct I/O for filesystem operations.
+    ///
+    /// If set, filesystem operations will use direct I/O bypassing the page cache.
+    #[arg(long, default_value_t = false)]
+    direct_io: bool,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -56,7 +62,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let storage: ReadableStorage = Arc::new(store::OpendalStore::new(operator));
 
     // Default filesystem store
-    let storage: ReadableStorage = Arc::new(FilesystemStore::new(args.path.clone())?);
+    let mut options = FilesystemStoreOptions::default();
+    options.direct_io(args.direct_io);
+    let storage: ReadableStorage = Arc::new(FilesystemStore::new_with_options(
+        args.path.clone(),
+        options,
+    )?);
 
     let array = zarrs::array::Array::open(storage.clone(), "/")?;
     // println!("{:#?}", array.metadata());
--- src/bin/zarrs_binary2zarr.rs
+++ src/bin/zarrs_binary2zarr.rs
@@ -13,7 +13,7 @@ use zarrs::{
     },
     array_subset::ArraySubset,
     config::global_config,
-    filesystem::FilesystemStore,
+    filesystem::{FilesystemStore, FilesystemStoreOptions},
     metadata::v3::MetadataV3,
     storage::ListableStorageTraits,
 };
@@ -85,6 +85,12 @@ struct Cli {
 
     /// The output directory for the zarr array.
     out: PathBuf,
+
+    /// Enable direct I/O for filesystem operations.
+    ///
+    /// If set, filesystem operations will use direct I/O bypassing the page cache.
+    #[arg(long, default_value_t = false)]
+    direct_io: bool,
     // /// The path to a binary file or a directory of binary files.
     // #[arg(short, long, num_args = 1..)]
     // file: Vec<PathBuf>,
@@ -212,7 +218,9 @@ fn main() -> anyhow::Result<()> {
 
     // Create storage
     let path_out = cli.out.as_path();
-    let store = std::sync::Arc::new(FilesystemStore::new(path_out).unwrap());
+    let mut options = FilesystemStoreOptions::default();
+    options.direct_io(cli.direct_io);
+    let store = std::sync::Arc::new(FilesystemStore::new_with_options(path_out, options).unwrap());
 
     // Create array
     let dimension_names = cli
